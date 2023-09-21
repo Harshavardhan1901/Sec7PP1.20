64a01fbc8982a37843ce1a6ecf3ec9148a52e860
harsha1901@yahoo.com <harsha1901@yahoo.com>
2023-09-21 19:25:33 +0530

    PP

---
+++ BankWebApp/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace BankWebApp.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Username is required")]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Password is required")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
